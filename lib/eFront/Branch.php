cfa3ba35afa38426ffafcdd5efe1648ff2d4b491
Tyler Harms <tyler.harms@gmail.com>
2016-06-07 14:18:14 -0500

    Add missing job id to assignUserToBranch

--- lib/eFront/Branch.php
+++ lib/eFront/Branch.php
@@ -8,8 +8,7 @@ use eFront\Exception\JobException;
 use SimpleXMLElement;
 
 /**
- * Class Branch
- * @package eFront
+ * Class Branch.
  */
 class Branch extends eFront
 {
@@ -17,18 +16,29 @@ class Branch extends eFront
      * Assigns a user to a branch. The module checks whether the provided token is valid and whether its status is logged. If so, it processes the request and assigns the user to the branch.
      *
      * @link http://wiki.efrontlearning.net/XML_API2#User_to_Branch
-     * @param string $token token to communicate with the XML API module
-     * @param string $login the login of the corresponding user
-     * @param int $branchId
-     * @param int $positionId
+     *
+     * @param string $token          token to communicate with the XML API module
+     * @param string $login          the login of the corresponding user
+     * @param int    $branchId
+     * @param int    $jobId          The job ID (must not be 0)
+     * @param int    $positionId     The Position Id (0 for user, 1 for supervisor)
      * @param string $jobDescription
+     *
      * @return SimpleXMLElement
+     *
      * @throws Exception\ApiError
      * @throws Exception\ClientException
      */
-    public function assignUserToBranch($token, $login, $branchId, $positionId, $jobDescription)
+    public function assignUserToBranch($token, $login, $branchId, $jobId, $positionId, $jobDescription)
     {
-        $xml_response = $this->buildResponse($this->apiBase . "?action=user_to_branch" . "&token=" . $token . "&login=" . $login . '&branch=' . $branchId . '&position=' . $positionId . '&job_description=' . $jobDescription);
+        if ($jobId === 0) {
+            throw new JobException('Job ID must not be zero');
+        }
+        if ($positionId !== 0 && $positionId !== 1) {
+            throw new BranchException('Position ID only allows 1 or 0');
+        }
+        $xml_response = $this->buildResponse($this->apiBase . '?action=user_to_branch' . '&token=' . $token . '&login=' . $login . '&job=' . $jobId . '&branch=' . $branchId . '&position=' . $positionId . '&job_description=' . $jobDescription);
+
         return $xml_response;
     }
 
@@ -36,20 +46,23 @@ class Branch extends eFront
      * Get all the available jobs of a branch. The module checks whether the provided token is valid and whether its status is logged. If so, it processes the request and all the available jobs of the corresponding branch.
      *
      * @link http://wiki.efrontlearning.net/XML_API2#Branch_Jobs
+     *
      * @param $token
      * @param $login
-     * @param int $branchId
-     * @param null|int $jobId The job ID (must not be 0)
-     * @param null|int $positionId The Position Id (0 for user, 1 for supervisor)
+     * @param int         $branchId
+     * @param null|int    $jobId          The job ID (must not be 0)
+     * @param null|int    $positionId     The Position Id (0 for user, 1 for supervisor)
      * @param null|string $jobDescription
+     *
      * @return Exception\ApiError|string
+     *
      * @throws BadRequestException
      * @throws Exception\ApiError
      * @throws Exception\ClientException
      */
     public function getBranchJobs($token, $login, $branchId, $jobId = null, $positionId = null, $jobDescription = null)
     {
-        $requestUrl = $this->apiBase . "?action=branch_jobs" . "&token=" . $token . "&login=" . $login . '&branch=' . $branchId;
+        $requestUrl = $this->apiBase . '?action=branch_jobs' . '&token=' . $token . '&login=' . $login . '&branch=' . $branchId;
 
         if ($jobId !== null && is_int($jobId)) {
             if ($jobId === 0) {
@@ -68,6 +81,7 @@ class Branch extends eFront
         }
 
         $xml_response = $this->buildResponse($requestUrl);
+
         return $xml_response;
     }
 }
